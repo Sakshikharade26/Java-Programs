5d20f252b5ad3c8793c3070fc3856e7d29fbd657
Sakshi Kharade <sakshikharade0282@gmail.com>
2024-12-11 22:51:54 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,20 @@
+// Find length of Stri
+
+import java.util.*;
+
+public class StringLength 
+{
+   
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the String : ");
+        String str = sc.nextLine();
+
+        System.out.println("Length of String is : " + str.length());
+       
+
+    }
+    
+}
