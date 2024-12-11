ecda6a75d7e9feec8628db5e543c55524588a7c1
Sakshi Kharade <sakshikharade0282@gmail.com>
2024-12-11 23:07:53 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,16 @@
+//find average of number
+import java.util.*;
+public class Average
+{
+    public void Demo(int num)
+    {
+      int sum;
+      int count;
+
+      while(num>0)
+      {
+         sum = num/count;
+      }
+    }
+    
+}
